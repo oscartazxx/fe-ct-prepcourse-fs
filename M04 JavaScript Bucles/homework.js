67a5297e6a45f4c5b36d6ad1e911bc1edd06c692
oscartazxx <oscarj.jaramillo@outlook.com>
2023-03-12 17:16:05 -0500

    M04 hasta operadores logicos

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -100,12 +100,22 @@ function esDiezOCinco(num) {
    // Retornar true si "num" es 10 o 5.
    // De lo contrario, retornar false.
    // Tu código:
+   if (num==10 || num == 5){
+      return(true);      
+   }else{
+      return(false);
+   }
 }
 
 function estaEnRango(num) {
    // Retornar true si "num" es menor que 50 y mayor que 20.
    // De lo contrario, retornar false.
    // Tu código:
+   if (num> 20 && num < 50){
+      return(true);      
+   }else{
+      return(false);
+   }
 }
 
 function esEntero(num) {
@@ -115,7 +125,15 @@ function esEntero(num) {
    // Ejemplo: (-10) ---> true
    // De lo contrario, retorna false.
    // Tu código:
+   var res;
+if (Number.isInteger(num)===true){
+   res=true;
+  }else{
+   res=false;
+  }
+  return(res);
 }
+
 
 function fizzBuzz(num) {
    // Si "num" es divisible entre 3, retorna "fizz".
@@ -123,6 +141,15 @@ function fizzBuzz(num) {
    // Si "num" es divisible entre 3 y 5 (ambos), retorna "fizzbuzz".
    // De lo contrario, retorna false.
    // Tu código:
+   if (num%3==0 && num%5!=0){
+      return("fizz");
+   }else if(num%5==0 && num%3 !=0){
+      return("buzz");
+   }else if(num%5==0 && num%3 ==0){
+      return("fizzbuzz");
+   }else {
+      return(false);
+   }
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -133,6 +160,18 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
+   if (num1>num2 && num1>num3 && num1%2==0){
+      return("Numero 1 es mayor y positivo");
+   }else if (num1<0 || num2<0 || num3<0){
+      return("Hay negativos");
+   }else if(num3>num2 && num3> num1){
+      num3++;
+      return(num3);
+   }else if(num1==0 && num2 == 0 && num3 == 0){
+      return("Error");
+   }else{
+      return(false)
+   }
 }
 
 function esPrimo(num) {
@@ -142,7 +181,29 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
+   var noprimo = 0;
+   var resultado; 
+   if (num==0 || num ==1){
+      noprimo++;
+      //return("No es primo");
+   }else{
+      for (var i=2; i<num; i++){
+         if (num%i==0) {      //si es divisible exactamente no es primo
+           noprimo++;             
+         }
+      }
+   }  
+   if (noprimo=0){
+      resultado=true;
+   } else{
+      resultado=false;      
+   }
+   return(resultado);
 }
+
+  
+
+
 
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
