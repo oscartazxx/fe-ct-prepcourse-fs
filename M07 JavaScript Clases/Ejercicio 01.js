654fcdb4219024b1e4a51fe92bc7a3cd2c731d3b
oscartazxx <oscarj.jaramillo@outlook.com>
2023-03-30 21:26:34 -0500

    finalizado ejercicio 1 modulo 7

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -1,7 +1,7 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*1️⃣ EJERCICIO 01 1️⃣*/
 
-function crearUsuario() {
+function crearUsuario(usuario, nombre, email, password) {
    // Crea una Clase de ES6 o una función constructora llamada "Usuario".
    // El constructor de esta clase debe tener las propiedades: "usuario", "nombre", "email" y "password".
    // El valor de cada propiedad la recibirá por parámetro.
@@ -10,19 +10,60 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+  
+      
+   return(Usuario);
+   
 }
+
+class Usuario {
+   constructor(usuario, nombre, email, password){
+      this.usuario = usuario;
+      this.nombre = nombre;
+      this.email=email;         
+      this.password = password;
+   }
+   saludar(){
+      return('Hola, mi nombre es ' + this.nombre)
+   }
+}
+
+
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   Usuario.prototype.saludar = function (){
+      return('Hello World!');
+   }
+
+   return(Usuario.saludar);
+
+
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+ const str = 'Hola'
+ return(str.reverse);
+
 }
+
+String.prototype.reverse = function () {
+   //let arreglo = [];
+   let separado = this.split("");
+   return separado.reverse().join("");
+
+   // separado.forEach((letra) => {
+   //   arreglo.unshift(letra);
+   // });
+   // let palabraFinal = arreglo.join("");
+   // return this.palabraFinal;
+ };
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
